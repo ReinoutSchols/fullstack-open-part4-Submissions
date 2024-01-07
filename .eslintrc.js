ebf330fbc536cd822d64cb4a4eb968f32c26c159
Reinout Schols <reinoutschols@hotmail.com>
2024-01-07 15:02:47 +0100

    completed exercises 4.1 and 4.2

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    node: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    indent: [
+      'error',
+      2,
+    ],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always',
+    ],
+    'no-console': 0,
+  },
+};
